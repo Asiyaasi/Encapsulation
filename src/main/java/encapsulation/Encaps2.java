a49fe4f5daa11a92709c8a8f467f9f659e0a35a6
asiya <asiya@LAPTOP-MLC4OS7H>
2024-11-13 12:26:46 +0530

    commit

---
+++ src/main/java/encapsulation/Encaps2.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class Encaps2 {
+
+	public static void main(String[] args) {
+		Encasps1 obj1=new Encasps1();
+		obj1.setId(12);
+		obj1.setMark(45);
+		System.out.println(obj1.getId());
+		System.out.println(obj1.getMark());
+	}
+
+}
---
+++ src/main/java/encapsulation/Encasps1.java
@@ -0,0 +1,20 @@
+package encapsulation;
+
+public class Encasps1 {
+	private int id;
+	private int mark;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getMark() {
+		return mark;
+	}
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+	
+
+}
